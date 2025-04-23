fcc1ab83814871e309bde1d0c15fed1beb60dfea
Lana150 <astra.altair150@gmail.com>
2025-04-24 01:51:09 +0200

    collections and databases

---
+++ src/models/Transaction.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const transactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: { type: String, enum: ['income', 'expense'], required: true },
+    category: { type: String, required: true },
+    sum: { type: Number, required: true },
+    comment: { type: String, default: '' },
+    date: { type: Date, default: Date.now },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const Transaction = mongoose.model('Transaction', transactionSchema);
