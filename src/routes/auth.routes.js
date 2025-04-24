d28e41a592fff3d13057c54dc8043453453087fa
dimondesh <dimondesh1337@gmail.com>
2025-04-24 19:09:37 +0300

    commit

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import {ctrlWrapper} from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validate.middleware.js';
+import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+
 
 import { registerController, loginController, logoutController } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 const jsonParser = express.json();
 
-router.post("/register", jsonParser, ctrlWrapper(registerController));
-router.post("/login", jsonParser, ctrlWrapper(loginController));
+router.post("/register", jsonParser,validateBody(registerUserSchema), ctrlWrapper(registerController));
+router.post("/login", jsonParser, validateBody(loginUserSchema),ctrlWrapper(loginController));
 router.post("/logout", jsonParser, ctrlWrapper(logoutController));
 
 export default router;
