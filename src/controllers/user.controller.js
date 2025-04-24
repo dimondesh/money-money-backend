515f8c0eaea3aee659f1fc9af1670d4f01931dfd
Adriana <adrianat9099@gmail.com>
2025-04-24 15:34:17 +0300

    userRoute, ctrlWrapper

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import { getCurrentUser } from '../services/user.service';
+
+export const getCurrentUserController = async (req, res) => {
+  const user = await getCurrentUser(req.userId);
+
+  res.json({
+    status: 200,
+    message: 'User info retrieved',
+    data: user,
+  });
+};
